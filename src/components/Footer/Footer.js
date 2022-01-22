d2bc3e93f79304c5a1b04585822d6734a5ee2e41
Rashika258 <rashikasuresh5758@gmail.com>
2022-01-22 10:32:30 +0530

    changes

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -7,18 +7,18 @@ const Footer = () => {
       <hr style={{ width: "90%", marginTop: 20 }} />
       <span className="name">
         Made by{" "}
-        <a href="https://www.youtube.com/c/RoadsideCoder" target="__blank">
-          Roadside Coder
+        <a href="https://www.youtube.com/" target="__blank">
+          Rashika
         </a>
       </span>
       <div className="iconContainer">
-        <a href="https://www.instagram.com/roadsidecoder/" target="__blank">
+        <a href="https://www.instagram.com/" target="__blank">
           <i className="fab fa-instagram-square fa-2x"></i>
         </a>
-        <a href="https://www.linkedin.com/in/piyush-eon" target="__blank">
+        <a href="https://www.linkedin.com/in/" target="__blank">
           <i className="fab fa-linkedin fa-2x"></i>
         </a>
-        <a href="https://www.youtube.com/c/RoadsideCoder" target="__blank">
+        <a href="https://www.youtube.com/" target="__blank">
           <i className="fab fa-youtube fa-2x"></i>
         </a>
       </div>
